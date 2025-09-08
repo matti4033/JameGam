1be528be435e18c6d2d2e537eb8460773def756d
Chrixie <Chrixie@hotmail.com>
2025-09-08 19:41:31 +0200

    fixed bugs & added tutorial

--- JameGam/Assets/Scripts/MapChanges/UI.cs
+++ JameGam/Assets/Scripts/MapChanges/UI.cs
@@ -8,16 +8,29 @@ public class UI : MonoBehaviour
     public GameObject goLevel2;
     public GameObject pauseMenu;
 
+    public GameObject tutorialText;
+    public float tutorialTimer = 20f;
+
     void Start()
     {
         UiText.SetActive(false);
         goLevel1.SetActive(false);
         goLevel2.SetActive(false);
+
+        if (GameManager.Instance.bossesdead == 0)
+            tutorialText.SetActive(true);
+        else
+            tutorialText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        tutorialTimer -= Time.deltaTime;
 
+        if (tutorialTimer < 0)
+        {
+            tutorialText.SetActive(false);
+        }
     }
 }
